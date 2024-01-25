5859474969871abaea6ab8062abb70a474b0ff10
praveenkumar <praveenkumarramesht@gmail.com>
2024-01-25 14:26:15 +0530

    Add project files.

---
+++ EntityFramework/IStudentsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    public interface IStudentsRepository
+    {
+        public void Inser(StudentDetails stud);
+        public void Delete(long id);
+        public void Update(long id, StudentDetails stud);
+        public IEnumerable<StudentDetails> GetStudentDetails();
+        public StudentDetails GetbyID(long StudentID);
+    }
+}
---
+++ EntityFramework/StudentsReository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    public class StudentsReository : IStudentsRepository
+    {
+        private readonly Dbcontxt _contxt;
+        public StudentsReository(Dbcontxt contxt)
+        {
+            _contxt = contxt;
+        }
+        public void Insert(StudentDetails stud)
+        {
+            try
+            {
+                _contxt.Database.ExecuteSqlRaw($"exec Insertdetail '{stud.Name}','{stud.DOB}',{stud.Age},'{stud.Gender}',{stud.MobileNum},'{stud.Emailid}','{stud.Subject}'");
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+        public void Update(long id, StudentDetails stud)
+        {
+            try
+            {
+                var result = _contxt.Database.ExecuteSqlRaw($" update StudDetail set Name='{stud.Name}',DOB='{stud.DOB}',Age={stud.Age},Gender='{stud.Gender}',MobileNum='{stud.MobileNum}',Emailid='{stud.Emailid}',Subject='{stud.Subject}' where StudentID={id}");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public void Delete(long studentid)
+        {
+            try
+            {
+                _contxt.Database.ExecuteSqlRaw($"delete StudDetail where StudentID={studentid}");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+    }
+}
